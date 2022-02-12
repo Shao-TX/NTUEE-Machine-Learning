fdf6973b3ad0602c1d6773d6f15cd642b3925610
ShaoTX <shao88014@gmail.com>
2022-02-12 19:26:47 +0800

    Changed enumerate to tqdm

--- HW2/testing.py
+++ HW2/testing.py
@@ -4,6 +4,9 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
+#
+from tqdm import tqdm
+
 # Data Preprocess
 import numpy as np
 import pandas as pd
@@ -44,12 +47,34 @@ class TIMITDataset(Dataset):
         return len(self.data)
 
 #%%
+#%%
+class TIMITDataset(Dataset):
+    def __init__(self, x, y, test_mode=False):
+        self.test_mode = test_mode
+
+        if(self.test_mode == False):
+            self.data  = torch.FloatTensor(x)
+            self.label = torch.LongTensor(y) # Beacuse CrossEntropy need Long type
+        else:
+            self.data  = torch.FloatTensor(x)
+    
+    def __getitem__(self, index):
+        if(self.test_mode == False):
+            return self.data[index], self.label[index]
+        else:
+            return self.data[index]
+    
+    def __len__(self):
+        return len(self.data)
+
+#%%
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.layer1   = nn.Linear(429 , 1024)
         self.layer2   = nn.Linear(1024, 512)
-        self.layer3   = nn.Linear(512 , 128)
+        self.layer3   = nn.Linear(512 , 256)
+        self.layer4   = nn.Linear(256 , 128)
         self.out      = nn.Linear(128 , 39)
 
         self.act_fn_1 = nn.Sigmoid()
@@ -57,11 +82,14 @@ class Net(nn.Module):
 
         self.BN1      = nn.BatchNorm1d(1024)
         self.BN2      = nn.BatchNorm1d(512)
-        self.BN3      = nn.BatchNorm1d(128)
+        self.BN3      = nn.BatchNorm1d(256)
+        self.BN4      = nn.BatchNorm1d(128)
 
         self.dropout  = nn.Dropout(p = 0.5) 
 
     def forward(self, x):
+        x = self.dropout(x)
+
         x = self.layer1(x)
         x = self.BN1(x)
         x = self.act_fn_2(x)
@@ -74,6 +102,10 @@ class Net(nn.Module):
         x = self.BN3(x)
         x = self.act_fn_2(x)
 
+        x = self.layer4(x)
+        x = self.BN4(x)
+        x = self.act_fn_2(x)
+
         x = self.out(x)
 
         return x
@@ -84,7 +116,7 @@ if __name__ == "__main__":
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
     # Model Path
-    model_name = input("Enter model name")
+    model_name = input("Enter model name : ")
     model_path = model_name + ".pth"
     save_path = model_name + "_submission.csv"
 
@@ -108,7 +140,7 @@ if __name__ == "__main__":
     model.eval()
     preds = []
     with torch.no_grad(): 
-        for i, data in enumerate(test_loader): 
+        for data in tqdm(test_loader): 
             data = data.to(device)
             pred = model(data)
 
@@ -121,4 +153,5 @@ if __name__ == "__main__":
         for i, y in enumerate(preds):
             f.write('{},{}\n'.format(i, y))
 
+    print("Save to Excel")
 #%%
